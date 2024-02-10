e6a0d10ded71b8a9ed611d3474ac2b37dda9b1cb
s1mpleog <mrzkk07@gmail.com>
2024-02-10 14:29:01 +0530

    created custom apiError and response and configured express

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import dotenv from "dotenv";
 import connectDB from "./config/database.js";
+import { app } from "./app.js";
+
+const PORT = process.env.PORT || 5000;
 
 dotenv.config();
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`SERVER IS RUNNING AT PORT ${PORT}`);
+    });
+  })
+  .catch((err) => console.error(`ERROR WHILE CONNECTING TO DB ${err.message}`));
